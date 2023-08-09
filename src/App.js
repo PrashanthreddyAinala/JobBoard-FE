acea54566f52a4119376e71f81b0c8074eb28ed4
Mohan Kumar Puram <mohankumarpuram@Mohans-MBP.lan>
2023-08-09 14:22:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p class="text-9xl font-bold underline">
+        Hello world!
+      </p>
+    </div>
+  );
+}
+
+export default App;
